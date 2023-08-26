3688fa4f634328621da35d54a7d1bf8ee9d7cf37
alihuseynaliyev <alihuseyneliyev12@gmail.com>
2023-08-27 00:34:38 +0400

    "base url change"

--- src/main/java/com/alinazim/ms/ticket/controller/TicketController.java
+++ src/main/java/com/alinazim/ms/ticket/controller/TicketController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("v1/tickets")
+@RequestMapping("internal/v1/tickets")
 @RequiredArgsConstructor
 public class TicketController {
     private final TicketService ticketService;
